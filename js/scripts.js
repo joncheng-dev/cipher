8fb8bdc029e07a68f59649966686e730ccfc0020
Jonathan Cheng <jcheng461@gmail.com>
2022-02-20 09:43:36 +0900

    set up css and js directories. Populate with bootstrap, styles, scripts.js, and jquery files. HTML set up link to JS scripts. Scripts file add function to prompt user to enter sentence. Shows what user entered.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function getSentence() {
+  const entered = prompt("Enter a sentence: ");
+  return entered;
+}
+
+const userEnterSentence = getSentence();
+
+alert("Sentence entered was: " + userEnterSentence);
